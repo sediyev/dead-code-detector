af9d7110968b865150dfba15ed8968aba0d247b5
Emin Sadiyev <sediyev@gmail.com>
2017-03-28 23:08:30 +0300

    spring boot application setup

--- src/main/java/com/aurea/DeadCodeGeneratorApplication.java
+++ src/main/java/com/aurea/DeadCodeGeneratorApplication.java
@@ -2,11 +2,28 @@ package com.aurea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class DeadCodeGeneratorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeadCodeGeneratorApplication.class, args);
+	}
+
+	@Bean
+	public Docket api(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.any())
+				.build();
+
 	}
 }
--- src/main/java/com/aurea/controller/DeadCodeDetector.java
+++ src/main/java/com/aurea/controller/DeadCodeDetector.java
@@ -1,45 +1,40 @@
 package com.aurea.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-import java.util.Collection;
-import java.util.Collections;
-
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static org.springframework.web.bind.annotation.RequestMethod.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Created by sediy on 3/28/2017.
  */
-@RestController
-@RequestMapping("/rest/v1/")
+@Component
+@RequestMapping("/rest/v1")
 public class DeadCodeDetector {
 
     @ResponseBody
-    @RequestMapping(value="/add", method = POST, produces = APPLICATION_JSON)
-    public String addRepositoryForInspection(@RequestParam String url){
+    @RequestMapping(value = "/add", method = POST, produces = APPLICATION_JSON_VALUE)
+    public String addRepositoryForInspection(@RequestParam String url) {
 
         throw new RuntimeException("Not yet implemented!");
     }
 
     @ResponseBody
-    @RequestMapping(value="/repositories", method = GET, produces = APPLICATION_JSON)
-    public ResponseEntity<String> getRepositories(){
+    @RequestMapping(value = "/repositories", method = GET, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getRepositories() {
 
         return ResponseEntity.ok("Not implemented yet");
     }
 
     @ResponseBody
-    @RequestMapping(value="/repositories/{id}", method = GET, produces = APPLICATION_JSON)
-    public ResponseEntity<String> getRepositories(@PathVariable("id") String id){
+    @RequestMapping(value = "/repositories/{id}", method = GET, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getRepositories(@PathVariable("id") String id) {
 
         return ResponseEntity.ok("It Will return repository detection results belonging to specific id. Not implemented yet");
     }
