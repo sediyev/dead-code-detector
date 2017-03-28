c1b417f800781437a4fbff2c44791cb0ccc52e88
Emin Sadiyev <sediyev@gmail.com>
2017-03-28 14:36:25 +0300

    second commit

--- src/main/java/com/aurea/JerseyConfig.java
+++ src/main/java/com/aurea/JerseyConfig.java
@@ -1,7 +1,12 @@
 package com.aurea;
 
+import org.glassfish.jersey.server.ResourceConfig;
+
 /**
  * Created by sediy on 3/28/2017.
  */
-public class JerseyConfig {
+public class JerseyConfig extends ResourceConfig{
+    public JerseyConfig(){
+//        register()
+    }
 }
