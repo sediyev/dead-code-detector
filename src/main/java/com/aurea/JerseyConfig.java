36b547a0127172ca0d63bd4d2c7fe7990661cc1b
Emin Sadiyev <sediyev@gmail.com>
2017-03-28 17:40:55 +0300

    jersey configuration and imports in build.gradle

--- src/main/java/com/aurea/JerseyConfig.java
+++ src/main/java/com/aurea/JerseyConfig.java
@@ -1,12 +1,15 @@
 package com.aurea;
 
+import com.aurea.controller.DeadCodeDetector;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by sediy on 3/28/2017.
  */
+@Configuration
 public class JerseyConfig extends ResourceConfig{
     public JerseyConfig(){
-//        register()
+        register(DeadCodeDetector.class);
     }
 }
