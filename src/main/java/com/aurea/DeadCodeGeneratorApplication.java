198930e2c0bb71428445fa72771df48e771170d4
Emin Sadiyev <sediyev@gmail.com>
2017-03-29 00:29:59 +0300

    extra detail for swagger-ui

--- src/main/java/com/aurea/DeadCodeGeneratorApplication.java
+++ src/main/java/com/aurea/DeadCodeGeneratorApplication.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -29,8 +31,21 @@ public class DeadCodeGeneratorApplication {
                 .select()
                 .paths(Predicates.not(PathSelectors.regex("/error")))
                 .apis(RequestHandlerSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
     }
 
+    private ApiInfo apiInfo(){
+        Contact contact = new Contact("Emin Sadiyev", "", "");
+        ApiInfo apiInfo = new ApiInfo(
+                "Dead Code Detector",
+                "This API crawls given github repositories and detects dead code among them.",
+                "v1.0",
+                "",
+                contact,
+                "",
+                "");
 
+        return apiInfo;
+    }
 }
