13ab6e67fc8991b621b5c3dc849e79f1a0918522
Emin Sadiyev <emin.sadiyev@aurea.com>
2017-03-28 14:26:13 +0300

    first commit

---
+++ src/main/java/com/aurea/DeadCodeGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.aurea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadCodeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeadCodeGeneratorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aurea/DeadCodeGeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aurea;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeadCodeGeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
