abdd3b13a1f497ddfa1bd764aefb302bd21c5a8d
Emin Sadiyev <sediyev@gmail.com>
2017-03-29 11:33:25 +0300

    Google Java Style is imported to idea

--- src/main/java/com/aurea/DeadCodeGeneratorApplication.java
+++ src/main/java/com/aurea/DeadCodeGeneratorApplication.java
@@ -1,11 +1,9 @@
 package com.aurea;
 
-import com.aurea.controller.DeadCodeDetector;
 import com.google.common.base.Predicates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -16,36 +14,33 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackageClasses = {
-        DeadCodeDetector.class
-})
 public class DeadCodeGeneratorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DeadCodeGeneratorApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DeadCodeGeneratorApplication.class, args);
+  }
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .paths(Predicates.not(PathSelectors.regex("/error")))
-                .apis(RequestHandlerSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .paths(Predicates.not(PathSelectors.regex("/error")))
+        .apis(RequestHandlerSelectors.any())
+        .build()
+        .apiInfo(apiInfo());
+  }
 
-    private ApiInfo apiInfo(){
-        Contact contact = new Contact("Emin Sadiyev", "", "");
-        ApiInfo apiInfo = new ApiInfo(
-                "Dead Code Detector",
-                "This API crawls given github repositories and detects dead code among them.",
-                "v1.0",
-                "",
-                contact,
-                "",
-                "");
+  private ApiInfo apiInfo() {
+    Contact contact = new Contact("Emin Sadiyev", "", "");
+    ApiInfo apiInfo = new ApiInfo(
+        "Dead Code Detector",
+        "This API crawls given github repositories and detects dead code among them.",
+        "v1.0",
+        "",
+        contact,
+        "",
+        "");
 
-        return apiInfo;
-    }
+    return apiInfo;
+  }
 }
--- src/main/java/com/aurea/controller/DeadCodeDetector.java
+++ src/main/java/com/aurea/controller/DeadCodeDetector.java
@@ -1,15 +1,15 @@
 package com.aurea.controller;
 
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by sediy on 3/28/2017.
@@ -18,25 +18,25 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RequestMapping("/rest/v1")
 public class DeadCodeDetector {
 
-    @ResponseBody
-    @RequestMapping(value = "/add", method = POST, produces = APPLICATION_JSON_VALUE)
-    public String addRepositoryForInspection(@RequestParam String url) {
+  @ResponseBody
+  @RequestMapping(value = "/add", method = POST, produces = APPLICATION_JSON_VALUE)
+  public String addRepositoryForInspection(@RequestParam String url) {
 
-        throw new RuntimeException("Not yet implemented!");
-    }
+    throw new RuntimeException("Not yet implemented!");
+  }
 
-    @ResponseBody
-    @RequestMapping(value = "/repositories", method = GET, produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> getRepositories() {
+  @ResponseBody
+  @RequestMapping(value = "/repositories", method = GET, produces = APPLICATION_JSON_VALUE)
+  public ResponseEntity<String> getRepositories() {
 
-        return ResponseEntity.ok("Not implemented yet");
-    }
+    return ResponseEntity.ok("Not implemented yet");
+  }
 
-    @ResponseBody
-    @RequestMapping(value = "/repositories/{id}", method = GET, produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> getRepositoryById(@PathVariable("id") String id) {
+  @ResponseBody
+  @RequestMapping(value = "/repositories/{id}", method = GET, produces = APPLICATION_JSON_VALUE)
+  public ResponseEntity<String> getRepositoryById(@PathVariable("id") String id) {
 
-        return ResponseEntity.ok("It Will return repository detection results belonging to specific id. Not implemented yet");
-    }
+    return ResponseEntity.ok("Not Yet Implemented.");
+  }
 
 }
