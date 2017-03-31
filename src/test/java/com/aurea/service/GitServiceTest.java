2c2cea8e6becfa687785051800c80902e02d72c4
Emin Sadiyev <sediyev@gmail.com>
2017-03-31 10:29:21 +0300

    removed spring dependency from this unit test, and added Ignore annotation

--- src/test/java/com/aurea/service/GitServiceTest.java
+++ src/test/java/com/aurea/service/GitServiceTest.java
@@ -6,21 +6,19 @@ import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class GitServiceTest {
 
-  @Autowired
-  GitService gitService;
+  GitService gitService = new GitService();
 
+  // TODO give local path for this directory
   private final String localGitRepository = "d:\\delete";
   private final File localDir = new File(localGitRepository);
+  private final String repoUrl = "https://github.com/sediyev/dead-code-detector.git";
 
   @Before
   public void setup() throws IOException {
@@ -34,12 +32,14 @@ public class GitServiceTest {
     FileUtils.cleanDirectory(localDir);
   }
 
+  // As cloning git repository from Github is slow; this test is not intended for automatic tests
+  @Ignore("Takes too long")
   @Test
   public void cloneRepositoryTest() throws GitAPIException, IOException {
 
-    gitService.cloneRepo("https://github.com/eclipse/jgit.git",localDir,"master");
+    System.out.println("Cloning git repository to local directory");
+    gitService.cloneRepo(repoUrl,localDir,"master");
 
-    System.out.println("Created directory");
 
   }
 
