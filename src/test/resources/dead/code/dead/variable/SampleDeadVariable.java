2a1fab68ccb55accd8f575495869cb722245adb7
Emin Sadiyev <sediyev@gmail.com>
2017-04-02 17:19:06 +0300

    dead-code resources v0.1

---
+++ src/test/resources/dead/code/dead/variable/SampleDeadVariable.java
@@ -0,0 +1,17 @@
+package dead.code.dead.variable;
+
+public class SampleDeadVariable{
+  private int unused;
+
+  private int used;
+
+  private int unused2;
+
+  private void use(){
+    unused = 1;
+
+    used = 2;
+
+    unused = used;
+  }
+}
