284a065b351bc5dd41aed892f8c03beee86f8c5e
Emin Sadiyev <sediyev@gmail.com>
2017-04-05 07:17:30 +0300

    test

--- src/test/java/com/aurea/service/UnderstandServiceTest.java
+++ src/test/java/com/aurea/service/UnderstandServiceTest.java
@@ -1,8 +1,36 @@
-import static org.junit.Assert.*;
+package com.aurea.service;
 
-/**
- * Created by sediy on 4/1/2017.
- */
-public class UnderstandServiceTest {
+import com.aurea.model.UnusedUnderstandEntity;
+import com.aurea.service.finder.AbstractDeadCodeFinderTest;
+import com.scitools.understand.Understand;
+import com.scitools.understand.UnderstandException;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.util.CollectionUtils;
+
+public class UnderstandServiceTest extends AbstractDeadCodeFinderTest {
+
+  @Test
+  public void findAll()
+      throws UnderstandException, InterruptedException, IOException, TimeoutException {
+
+    String deadCodeRoot = "deadcode";
+
+    understandService.createUdbDatabase(udbPath, getFullResourcePath(deadCodeRoot));
+    db = Understand.open(udbPath);
+
+    List<UnusedUnderstandEntity> allDeadCodes = understandService
+        .findAllDeadCode(db, tempDir.getAbsolutePath());
+
+    db.close();
+
+    Assert.assertNotNull(allDeadCodes);
+    Assert.assertFalse(CollectionUtils.isEmpty(allDeadCodes));
+
+    allDeadCodes.forEach(System.out::println);
+  }
 
 }
