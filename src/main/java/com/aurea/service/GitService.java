b0e8bfc10c9779f1f031e3ca7765655a40eab822
Emin Sadiyev <sediyev@gmail.com>
2017-04-06 15:46:50 +0300

    refactoring

--- src/main/java/com/aurea/service/GitService.java
+++ src/main/java/com/aurea/service/GitService.java
@@ -1,15 +1,35 @@
 package com.aurea.service;
 
+import com.aurea.exception.GitHubDownloadException;
 import com.aurea.model.DeadCodeDetection;
 import com.aurea.model.GitHubRepoUrl;
+import com.aurea.model.state.DownloadingRepoState;
 import java.io.File;
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GitService {
+
+  private static final Logger LOGGER = LoggerFactory
+      .getLogger(MethodHandles.lookup().lookupClass());
+
+  void downloadRepo(DeadCodeDetection deadCodeDetection, File localDir) {
+
+    LOGGER.info("Downloading repository for :{}", deadCodeDetection.getId());
+    deadCodeDetection.setState(new DownloadingRepoState());
+
+    try {
+      cloneRepo(deadCodeDetection, localDir);
+    } catch (GitAPIException | IOException e) {
+      throw new GitHubDownloadException(e.getMessage());
+    }
+  }
 
   void cloneRepo(DeadCodeDetection deadCodeDetection, File localDir)
       throws GitAPIException, IOException {
