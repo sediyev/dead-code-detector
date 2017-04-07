ea2b2c56c28d14400dcae0abec36f3743b930f7b
Emin Sadiyev <sediyev@gmail.com>
2017-04-07 23:22:07 +0300

    branch added

--- src/test/java/com/aurea/util/AbstractDeadCodeDetectionTest.java
+++ src/test/java/com/aurea/util/AbstractDeadCodeDetectionTest.java
@@ -7,9 +7,10 @@ public abstract class AbstractDeadCodeDetectionTest {
   protected DeadCodeDetection deadCodeDetection = createDeadCodeDetection();
 
   protected final String repoUrl = "https://github.com/sediyev/dead-code-detector.git";
+  private static final String BRANCH = "master";
 
   private DeadCodeDetection createDeadCodeDetection() {
-    DeadCodeDetection deadCodeDetection = new DeadCodeDetection(repoUrl);
+    DeadCodeDetection deadCodeDetection = new DeadCodeDetection(repoUrl, BRANCH);
     deadCodeDetection.setId(1L);
 
     return deadCodeDetection;
