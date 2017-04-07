3f4b31ae318cc806f5082c58f2b43ec994372cb7
Emin Sadiyev <sediyev@gmail.com>
2017-04-07 22:45:41 +0300

    branch added for git checkout options

--- src/main/java/com/aurea/model/GitHubRepo.java
+++ src/main/java/com/aurea/model/GitHubRepo.java
@@ -7,22 +7,24 @@ import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class GitHubRepoUrl {
+public class GitHubRepo {
 
   private static Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   private URL repoUrl;
+  private String branch;
 
-  GitHubRepoUrl(String urlValue) {
+  GitHubRepo(String urlValue, String branch) {
 
     this.repoUrl = parseURL(urlValue);
+    this.branch = branch;
   }
 
   private URL parseURL(String urlValue) {
     try {
       return new URL(urlValue);
     } catch (MalformedURLException e) {
-      LOGGER.error("error parsing url: "+urlValue, e);
+      LOGGER.error("error parsing url: " + urlValue, e);
       throw new IllegalArgumentException("Input: " + urlValue + ", is not a valid url.");
     }
   }
@@ -32,8 +34,12 @@ public class GitHubRepoUrl {
     return repoUrl;
   }
 
+  public String getBranch() {
+    return branch;
+  }
+
   @Override
-  public String toString(){
+  public String toString() {
     return repoUrl.toString();
   }
 }
